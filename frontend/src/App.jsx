084b4cc56dc15b23796345124f7b33bec7a1f197
jefik <jansilak@seznam.cz>
2026-01-24 02:42:23 +0100

    feat: implement GeoJSON masking, Turf.js validation and map logic change
    - Added GeoJSON overlay with hole for czech republic
    - Turf.js for validation to restrict clicks within set borders
    - Implemented map locking (draggin, zoom) when creating pin
    - Refactored components, added useMemo optimization
    - Improved styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import MapLeaflet from "./components/MapLeaflet.jsx";
 
 function App() {
   return (
-    <div className="app-container" style={{ position: "relative", height: "100vh" }}>
-      {/* Welcome Overlay */}
+    <div className="app-container">
       <div className="welcome-message">
         <h2>Místa mluví</h2>
         <p>Mapa vzpomínek - Kliknutím na mapu zanecháte vzkaz</p>
--- frontend/src/components/MapLeaflet.jsx
+++ frontend/src/components/MapLeaflet.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState, useMemo } from "react";
 import "leaflet/dist/leaflet.css";
 import { usePins, useCreatePin } from "../hooks/usePins";
-import { MapContainer, TileLayer, useMapEvents, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents, useMap, GeoJSON } from "react-leaflet";
 import { Marker, Popup, Rectangle } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import czechGeoJSON from "/data/czech_republic.json";
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
+import { point as turfPoint, polygon as turfPolygon } from "@turf/helpers";
 
 function MapyLogo() {
   const map = useMap();
 
-  // Only add the control if it hasn't been added yet
+  // Add mapy.cz logo only if its not there
   if (!map.hasLogoControl) {
     const LogoControl = L.Control.extend({
       options: { position: "bottomleft" },
@@ -25,21 +28,20 @@ function MapyLogo() {
       },
     });
 
+    // Logo added
     new LogoControl().addTo(map);
-    map.hasLogoControl = true; // mark that logo was added
+    map.hasLogoControl = true;
   }
 
   return null;
 }
 
 export default function MapLeaflet() {
-  // Icons
+  // Custom icons
   const createCustomIcon = (mainColor, innerColor) => {
     return L.divIcon({
       className: "custom-pin",
       iconAnchor: [16, 32],
-      // popupAnchor [x, y]: 0 is centered horizontally,
-      // -32 moves the popup tip to the very top of the pin.
       popupAnchor: [0, -32],
       iconSize: [32, 32],
       html: `
@@ -51,64 +53,126 @@ export default function MapLeaflet() {
     });
   };
 
-  // Ikona pro uložené piny (Faded Copper s krémovým středem)
+  // Icon for saved pins (colors from scss)
   const savedPin = createCustomIcon("#b08968", "#ede0d4");
-
-  // Ikona pro náhled/rozepsaný pin (Tan s tmavším středem, aby se odlišil)
+  // Icon for draft pins (colors from scss)
   const previewPin = createCustomIcon("#7f5539", "#ede0d4");
 
-  // Pins
-  const textareaRef = useRef(null); // message formu pro přidanfi pinu
-  const [showForm, setShowForm] = useState(false);
-
-  // Hover modal
-  const [hoverPosition, setHoverPosition] = useState({ x: 0, y: 0 });
-  const [hoverPin, setHoverPin] = useState(null);
-
-  // Ref for the map instance
-  const mapRef = useRef(null);
+  // Pins message
+  const textareaRef = useRef(null);
 
   // Database requests
   const { data: pins = [] } = usePins();
   const createPinMutation = useCreatePin();
 
-  let isOverlay = true;
+  // Func to create black overlay with hole in shape of czech republic
+  const worldOverlay = useMemo(() => {
+    // Outer boundary (world boundary)
+    const worldCoords = [
+      [
+        [-180, 90], // North west
+        [-180, -90], // South west
+        [180, -90], // South east
+        [180, 90], // North east
+        [-180, 90], // Close
+      ],
+    ];
 
-  function MapClickHandler({ onClick }) {
+    // Extract coordinates of czech republic
+    const rawCzechCoords = czechGeoJSON.features[0].geometry.coordinates;
+
+    // Reverse czech republic coordinates to create hole
+    const czechHoles = [rawCzechCoords[0].toReversed()];
+
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        // First coords are outer, second coords are inside (the hole - czech republic)
+        coordinates: [...worldCoords, ...czechHoles],
+      },
+    };
+  }, []);
+
+  function MapClickHandler() {
     const map = useMap();
-
     const [draftPin, setDraftPin] = useState(null);
     const [showError, setShowError] = useState(false);
 
-    useMapEvents({
-      click(e) {
-        if (isOverlay == false) isOverlay = true;
-        else isOverlay = false;
+    // Func to unlock and lock dragging on map, if map havent been zoomed
+    const updateDragging = () => {
+      const zoom = map.getZoom();
+      if (zoom <= 8) {
+        map.dragging.disable();
+      } else if (!draftPin) {
+        map.dragging.enable();
+      }
+    };
 
+    // Check instantly if map wasnt zoomed on load
+    updateDragging();
+
+    // Locking unlocking map
+    const setMapLock = (isLocked) => {
+      const action = isLocked ? "disable" : "enable";
+      map.dragging[action]();
+      map.touchZoom[action]();
+      map.doubleClickZoom[action]();
+      map.scrollWheelZoom[action]();
+      map.boxZoom[action]();
+      map.keyboard[action]();
+
+      // + and - icons of map styling
+      const mapContainer = map.getContainer();
+      if (isLocked) {
+        mapContainer.classList.add("map-locked");
+      } else {
+        mapContainer.classList.remove("map-locked");
+      }
+    };
+
+    // Func to prevent clicking outside of czech republic
+    const czechBoundary = useMemo(() => {
+      const feature = czechGeoJSON.features[0].geometry.coordinates;
+      return turfPolygon(feature);
+    }, []);
+
+    useMapEvents({
+      zoomend: () => {
+        updateDragging();
+      },
+      // Create new draft pin on map click
+      click(e) {
         const lat = e.latlng.lat;
         const lng = e.latlng.lng;
 
-        const point = map.latLngToContainerPoint([lat, lng]);
-        const targetPoint = L.point(point.x, point.y - 150);
-        const targetLatLng = map.containerPointToLatLng(targetPoint);
+        const pt = turfPoint([lng, lat]);
+        // Check if click is outside of czech republic
+        if (!booleanPointInPolygon(pt, czechBoundary)) {
+          return;
+        }
 
-        map.panTo(targetLatLng, { animate: true, duration: 0.5 });
+        // Lock the map when pin is beeing made
+        setMapLock(true);
 
         setDraftPin({ lat, lng, message: "" });
         setShowError(false);
       },
+
+      // Escape handler to cancel pin creation
+      keydown(e) {
+        if (e.originalEvent.key === "Escape") {
+          setDraftPin(null);
+          setMapLock(false);
+          updateDragging();
+        }
+      },
     });
 
-    // Quit pin form by Esc key
-    const handleEsc = (e) => {
-      if (e.key === "Escape") {
-        setDraftPin(null);
-        isOverlay = true;
-      }
-    };
-    window.addEventListener("keydown", handleEsc);
+    // Dont render anything if pin is not beeing made
+    if (!draftPin) return null;
 
-    return draftPin === null || isOverlay ? null : (
+    return (
       <>
         <Marker
           key={`${draftPin.lat}-${draftPin.lng}`}
@@ -121,16 +185,27 @@ export default function MapLeaflet() {
           }}
         >
           <Popup closeButton={false} autoClose={false} closeOnClick={false} className="custom-form">
-            <div style={{ width: "300px" }} onClick={(e) => e.stopPropagation()}>
+            <div className="card-layout" onClick={(e) => e.stopPropagation()}>
               <div className="card shadow-lg">
                 <div className="card-header d-flex justify-content-between align-items-center">
-                  <strong className="small">Nová zpráva</strong>
+                  <h6 className="">Zanechte vzkaz</h6>
                   <button
-                    className="btn-close"
+                    className="btn-custom-close"
                     onClick={() => {
                       setDraftPin(null);
+                      setMapLock(false);
                     }}
-                  />
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                    </svg>
+                  </button>
                 </div>
 
                 <div className="card-body pt-0">
@@ -139,6 +214,7 @@ export default function MapLeaflet() {
                     rows={3}
                     autoFocus
                     ref={textareaRef}
+                    placeholder="Co se vám honí hlavou..."
                   />
 
                   <div className="d-flex justify-content-end gap-2 mt-2">
@@ -146,6 +222,7 @@ export default function MapLeaflet() {
                       className="btn btn-sm btn-secondary"
                       onClick={() => {
                         setDraftPin(null);
+                        setMapLock(false);
                       }}
                     >
                       Zrušit
@@ -165,6 +242,7 @@ export default function MapLeaflet() {
                           message: draftPin.message,
                         });
                         setDraftPin(null);
+                        setMapLock(false);
                       }}
                     >
                       Uložit
@@ -175,6 +253,7 @@ export default function MapLeaflet() {
             </div>
           </Popup>
         </Marker>
+
         <Rectangle
           bounds={[
             [45.0, 5.0],
@@ -187,8 +266,10 @@ export default function MapLeaflet() {
           }}
           eventHandlers={{
             click: (e) => {
-              e.originalEvent.stopPropagation();
+              // Prevent creating a new pin under the overlay
+              L.DomEvent.stopPropagation(e);
               setDraftPin(null);
+              setMapLock(false);
             },
           }}
         />
@@ -207,7 +288,24 @@ export default function MapLeaflet() {
         [51.06, 18.87],
       ]}
       className="dark-theme"
+      maxBoundsViscosity={1}
+      renderer={L.svg({ padding: 3 })} // Rendering more outside then loaded map
     >
+      <GeoJSON
+        data={worldOverlay}
+        style={{
+          weight: 2,
+          color: "#7f5539",
+          fillColor: "#0000009c",
+          interactive: true,
+          className: "geojson-nocursor",
+        }}
+        eventHandlers={{
+          click: (e) => {
+            L.DomEvent.stopPropagation(e); // Prevent creating new pin under the overlay
+          },
+        }}
+      />
       <TileLayer
         url={`https://api.mapy.com/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${import.meta.env.VITE_MAP_API_KEY}`}
         attribution='<a href="https://api.mapy.com/copyright" target="_blank">&copy; Seznam.cz</a>'
@@ -216,7 +314,19 @@ export default function MapLeaflet() {
       <MapClickHandler />
       <MarkerClusterGroup showCoverageOnHover={false}>
         {pins.map((pin) => (
-          <Marker key={pin.id} position={[pin.latitude, pin.longitude]} icon={savedPin}>
+          <Marker
+            key={pin.id}
+            position={[pin.latitude, pin.longitude]}
+            icon={savedPin}
+            eventHandlers={{
+              popupopen: (e) => {
+                const popup = e.popup.getElement();
+                if (popup) {
+                  popup.classList.add("custom-message");
+                }
+              },
+            }}
+          >
             <Popup closeButton={false} autoClose={false} closeOnClick={false}>
               <div className="pin-message">
                 <p>{pin.message}</p>
